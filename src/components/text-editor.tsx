493a0cd213e1f396b3d6afef2056273d7e3b78d1
suarasiy <sidiqindrajatiyusuf@gmail.com>
2021-02-09 03:43:24 +0800

    Markdown Editor React

---
+++ src/components/text-editor.tsx
@@ -0,0 +1,50 @@
+import './text-editor.css';
+import { useEffect, useState, useRef } from 'react';
+import MDEditor from '@uiw/react-md-editor';
+import Resizable from '../components/resizable';
+
+interface TextEditorProps {}
+
+const TextEditor: React.FC<TextEditorProps> = () => {
+  const ref = useRef<HTMLDivElement | null>(null);
+  const [editing, setEditing] = useState(false);
+  const [value, setValue] = useState('# Header');
+
+  useEffect(() => {
+    const listener = (event: MouseEvent) => {
+      if (
+        ref.current &&
+        event.target &&
+        ref.current.contains(event.target as Node)
+      ) {
+        console.log('element clicked inside of an editor.');
+        return;
+      }
+      console.log('element is not clicked inside of an editor.');
+      setEditing(false);
+    };
+    document.addEventListener('click', listener, { capture: true });
+
+    return () => {
+      document.removeEventListener('click', listener, { capture: true });
+    };
+  }, []);
+
+  if (editing) {
+    return (
+      <div className="text-editor" ref={ref}>
+        <MDEditor value={value} onChange={(value) => setValue(value || '')} />
+      </div>
+    );
+  }
+
+  return (
+    <div className="text-editor card" onClick={() => setEditing(true)}>
+      <div className="card-content">
+        <MDEditor.Markdown source={value} />
+      </div>
+    </div>
+  );
+};
+
+export default TextEditor;
